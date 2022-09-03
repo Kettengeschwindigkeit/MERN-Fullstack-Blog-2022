4ef37b0e221306fee377c6912d9fb33acc98c6a1
Kettengeschwindigkeit <666damned999@mail.ru>
2022-09-03 12:09:58 +0300

    user registration & authorization

---
+++ server/controllers/auth.js
@@ -0,0 +1,101 @@
+import User from '../models/User.js'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+// Register user
+export const register = async (req, res) => {
+    try {
+        const { username, password } = req.body
+
+        const isUsed = await User.findOne({ username })
+
+        if (isUsed) {
+            return res.json({
+                message: 'This username is already using'
+            })
+        }
+
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(password, salt)
+
+        const newUser = new User({
+            username,
+            password: hash
+        })
+
+        await newUser.save()
+
+        res.json({
+            newUser,
+            message: 'Registering successful'
+        })
+
+    } catch (error) {
+        res.json({ message: 'User create error' })
+    }
+}
+
+// Login user
+export const login = async (req, res) => {
+    try {
+        const { username, password } = req.body
+
+        const user = await User.findOne({ username })
+
+        if (!user) {
+            return res.json({
+                message: 'This username doesn\'t exist...'
+            })
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+
+        if (!isPasswordCorrect) {
+            return res.json({
+                message: 'Password is wrong'
+            })
+        }
+
+        const token = jwt.sign(
+            {
+                id: user._id
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: '30d' }
+        )
+
+        res.json({
+            token, user, message: 'Welcome!'
+        })
+
+    } catch (error) {
+        res.json({ message: 'Authorization error' })
+    }
+}
+
+// Get Me
+export const getMe = async (req, res) => {
+    try {
+        const user = await User.findById(req.userId)
+
+        if (!user) {
+            return res.json({
+                message: 'This username doesn\'t exist...'
+            })
+        }
+
+        const token = jwt.sign(
+            {
+                id: user._id
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: '30d' }
+        )
+
+        res.json({
+            user, token
+        })
+    } catch (error) {
+        res.json({ message: 'No access' })
+    }
+}
--- server/index.js
+++ server/index.js
@@ -1,14 +1,29 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import cors from 'cors'
+
+import authRoute from './routes/auth.js'
 
 const app = express()
 dotenv.config()
 
+// Constants
 const PORT = process.env.PORT
 const DB_USER = process.env.DB_USER
 const DB_PASSWORD = process.env.DB_PASSWORD
 const DB_NAME = process.env.DB_NAME
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+// Routes
+app.use('/api/auth', authRoute)
+
+app.get('/', (req, res) => {
+    return res.json({ message: 'Everything is ok' })
+})
 
 async function start() {
     try {

d24da8f5f205f914c43b4d042621801076b40b1c
Kettengeschwindigkeit <666damned999@mail.ru>
2022-09-30 17:41:27 +0300

    get post by id [back-end]

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -63,3 +63,15 @@ export const getAll = async (req, res) => {
         res.json({ message: "Something went wrong..."})
     }
 }
+
+// Get Post By Id
+export const getById = async (req, res) => {
+    try {
+        const post = await Post.findOneAndUpdate(req.params.id, {
+            $inc: { views: 1 }
+        })
+        res.json(post)
+    } catch (error) {
+        res.json({ message: "Something went wrong..."})
+    }
+}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { createPost, getAll } from '../controllers/posts.js'
+import { createPost, getAll, getById } from '../controllers/posts.js'
 import { checkAuth } from '../utils/checkAuth.js'
 
 const router = new Router()
@@ -11,5 +11,9 @@ router.post('/', checkAuth, createPost)
 // Get All Post
 // http://localhost:3002/api/posts
 router.get('/', getAll)
+
+// Get Post By Id
+// http://localhost:3002/api/posts/:id
+router.get('/:id', getById)
 
 export default router
